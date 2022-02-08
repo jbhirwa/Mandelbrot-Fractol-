216984fde02e416fd6f490526c3e64b0f542c768
John Habaguhirwa <jhabaguh@F3C020.42adel.org.au>
2022-02-08 16:18:36 +1030

    progress

--- Srcs/Mandelbrot.c
+++ Srcs/Mandelbrot.c
@@ -1,9 +1,6 @@
+#include <stdio.h>
 #include <mlx.h>
 #include <stdlib.h>
-#include <math.h>
-
-#define LWST_VAL 200
-#define HGHST_VAL 600
 
 typedef struct	s_data {
 	void	*img;
@@ -40,34 +37,58 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+typedef struct	s_complex
+{
+	double	a;
+	double	b;
+}				t_complex;
+
+t_complex square(t_complex cmpx, t_complex Zcmpx)
+{
+	double	temp;
+	
+	temp = (cmpx.a * cmpx.a) - (cmpx.b * cmpx.b) + Zcmpx.a ;
+	cmpx.b = 2.0 * cmpx.a * cmpx.b + Zcmpx.b;
+	cmpx.a = temp;
+	return(cmpx);
+}
+
+void	Mandelbrot(t_data *img)
+{
+	int	x = 0;//
+	int	y = 0;//
+	t_complex	c;//
+	t_complex	z;//
+
+	for (x = 0; x < 1080; x++)	
+	{
+		for (y = 0; y < 1080; y++)
+		{	
+			c.a = ((double)x -(1080/2))/(1080/4);		//a
+			c.b = ((double)y -(1080/2))/(1080/4);	//b
+			z.a = 0.0;
+			z.b = 0.0;
+			int it = 0;
+			while (it++ < 50 && (z.a * z.a) + (z.b * z.b) <= 4)
+					z = square(z, c);
+			if (it >= 49)//
+				my_mlx_pixel_put(img, x, y, 0x00FF0000);
+		}
+	}
+	//return (it);
+}
+
 int	main(void)
 {
 	t_data	img;
 	t_program	vars;
-	unsigned int colour = 0x00ff0000;
- 	
+
 	vars.mlx_pointer = mlx_init();
 	vars.window = mlx_new_window(vars.mlx_pointer, 1920, 1080, "Hello world!");
 	img.img = mlx_new_image(vars.mlx_pointer, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
-
-
-	for (int pos = LWST_VAL; pos <= HGHST_VAL; ++pos)
-	{
-		colour++;
-		int x = pos;
-		int y = LWST_VAL;
-		if ((x + y)%2 == 0)
-		{
-			y += (rand()%(100 - 1 + 1)) + 1;
-			x += (rand()%(100 - 1 + 1)) + 1;
-   			my_mlx_pixel_put(&img, x, y, 0x00FF0000 + (rand()%(767 - 0 + 0)) + 0);
-		}
-		else 
-			my_mlx_pixel_put(&img, x, y, 0x0000FF00);
-    	}	
-	
+	Mandelbrot(&img);
 	mlx_put_image_to_window(vars.mlx_pointer, vars.window, img.img, 0, 0);
 	mlx_hook(vars.window, 2, 1L<<0, close, &vars);
 	mlx_loop(vars.mlx_pointer);
